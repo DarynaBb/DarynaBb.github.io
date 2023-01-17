9c539a928214d32e30ce62e2ac535d0547fbbe33
DarynaBb <darinabilenko@gmail.com>
2023-01-17 20:30:52 +0100

    added etnodim-eng

---
+++ projects/etnodim-eng/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./src/styles/styles.css', './index.html', './cart.html'],
+  theme: {
+
+    extend: {
+      colors: {
+        'etno': '#243c5a'
+      }
+    },
+  },
+  plugins: [],
+}
+
+
+
+
+
+
